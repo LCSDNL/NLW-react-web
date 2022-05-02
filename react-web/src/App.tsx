c0638a20bd2fe4fcc2e7bcb09592621a61d977de
LCSDNL <lucasdanielrambo@gmail.com>
2022-05-02 17:38:05 -0300

    tailwind config/button style test

--- react-web/src/App.tsx
+++ react-web/src/App.tsx
@@ -5,16 +5,16 @@ interface ButtonProps{
 
 function Button(props: ButtonProps){
 
-  return <button>{props.text}</button>
+  return <button className="bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors">{props.text}</button>
 }
 
 
 function App() {
-  return <div>
+  return (<div className="flex gap-2">
     <Button text="Enviar" />
     <Button text="Ok" />
 
-  </div>
+  </div>)
 }
 
 export default App
