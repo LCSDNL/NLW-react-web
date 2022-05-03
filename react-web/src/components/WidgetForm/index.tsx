a3ab06582dd57eb0f104cabc472fc28681644a8b
LCSDNL <lucasdanielrambo@gmail.com>
2022-05-03 19:02:59 -0300

    back button

--- react-web/src/components/WidgetForm/index.tsx
+++ react-web/src/components/WidgetForm/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-import { CloseButton } from "../CloseButton";
 
 import bugImageUrl from "../../assets/bug.svg"
 import ideaImageUrl from "../../assets/idea.svg"
@@ -40,6 +39,9 @@ export type FeedbackType= keyof typeof feedbackTypes;
 export function WidgetForm(){
     const [feedbackType, setFeedbackType]= useState<FeedbackType | null>(null);
 
+    function handleRestartFeedback(){
+        setFeedbackType(null);
+    }
 
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -48,7 +50,10 @@ export function WidgetForm(){
             {!feedbackType ? (
                 <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
             ) : (
-                <FeedbackContentStep feedbackType={feedbackType}/>
+                <FeedbackContentStep 
+                feedbackType={feedbackType}
+                onFeedbackRestartRequested={handleRestartFeedback}
+                />
             )}
 
 
