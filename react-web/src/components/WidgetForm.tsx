5549e351347372a67909d58bfe83d11ebaf38cf0
LCSDNL <lucasdanielrambo@gmail.com>
2022-05-03 17:34:11 -0300

    useState feedbackType

--- react-web/src/components/WidgetForm.tsx
+++ react-web/src/components/WidgetForm.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
+
 import { CloseButton } from "./CloseButton";
 
 import bugImageUrl from "../assets/bug.svg"
 import ideaImageUrl from "../assets/idea.svg"
 import thoughtImageUrl from "../assets/thought.svg"
+
 
 const feedbackTypes={
     Bug:{
@@ -28,7 +31,12 @@ const feedbackTypes={
     },
 };
 
+type FeedbackType= keyof typeof feedbackTypes;
+
 export function WidgetForm(){
+    const [feedbackType, setFeedbackType]= useState<FeedbackType | null>(null);
+
+
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
@@ -43,7 +51,7 @@ export function WidgetForm(){
                         <button
                         key={key}
                         className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                        //onClick=""
+                        onClick={()=>setFeedbackType(key as FeedbackType)}
                         type="button"
                         >
                             <img src={value.image.source} alt={value.image.alt} />
