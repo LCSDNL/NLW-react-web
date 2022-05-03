31f024ff7284a7abe961dea7c64e58df29c1c4b3
LCSDNL <lucasdanielrambo@gmail.com>
2022-05-03 17:44:03 -0300

    refactor Widgetform

--- react-web/src/components/WidgetForm/index.tsx
+++ react-web/src/components/WidgetForm/index.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-import { CloseButton } from "./CloseButton";
+import { CloseButton } from "../CloseButton";
 
-import bugImageUrl from "../assets/bug.svg"
-import ideaImageUrl from "../assets/idea.svg"
-import thoughtImageUrl from "../assets/thought.svg"
+import bugImageUrl from "../../assets/bug.svg"
+import ideaImageUrl from "../../assets/idea.svg"
+import thoughtImageUrl from "../../assets/thought.svg"
 
 
 const feedbackTypes={
