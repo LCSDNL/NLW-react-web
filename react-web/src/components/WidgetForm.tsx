f8bbd4c4a0bdab277d397becd5f230e67b329374
LCSDNL <lucasdanielrambo@gmail.com>
2022-05-03 16:48:13 -0300

    add ObjectArray feedbackTypes

--- react-web/src/components/WidgetForm.tsx
+++ react-web/src/components/WidgetForm.tsx
@@ -1,5 +1,25 @@
 import { CloseButton } from "./CloseButton";
 
+import bugImageUrl from "../assets/bug.svg"
+import ideaImageUrl from "../assets/idea.svg"
+import thoughtImageUrl from "../assets/thought.svg"
+
+const feedbackTypes={
+    Bug:{
+        title: "Problema",
+        image: {
+            source: "",
+            alt: ""
+        }
+    },
+    IDEIA:{
+        title: "Ideia",
+    },
+    OTHER:{
+        title: "Outro",
+    },
+};
+
 export function WidgetForm(){
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -9,8 +29,11 @@ export function WidgetForm(){
                 <CloseButton/>
             </header>
 
+            <div className="flex py-8 gap-2 w-full">
+                <button></button>
+            </div>
 
-            <p>Hello World!</p>
+            
             
             
             <footer className="text-xs text-neutral-400">
