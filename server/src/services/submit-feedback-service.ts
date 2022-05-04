88eb8a10c1ae21fd1956567972cbdf643d2558df
LCSDNL <lucasdanielrambo@gmail.com>
2022-05-04 17:44:06 -0300

    SubmitFeedback service

---
+++ server/src/services/submit-feedback-service.ts
@@ -0,0 +1,29 @@
+
+import { FeedbacksRepository } from './../repositories/feedbacks-repository';
+
+
+
+interface SubmitFeedbackRequest{
+    type: string;
+    comment: string;
+    screenshot: string;
+}
+
+export class SubmitFeedback{
+
+    constructor(
+        private feedbacksRepository: FeedbacksRepository
+    ){}
+
+    async execute(req: SubmitFeedbackRequest){ 
+        const { type, comment, screenshot} = req;
+
+        await this.feedbacksRepository.create({
+            type,
+            comment,
+            screenshot,
+        }
+            
+        )
+     }
+}
