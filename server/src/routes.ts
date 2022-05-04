969806439e3280a74b5192f3146a3146070c853a
LCSDNL <lucasdanielrambo@gmail.com>
2022-05-04 17:25:16 -0300

    routers

---
+++ server/src/routes.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import nodemailer from 'nodemailer'
+import { prisma } from './prisma';
+
+
+export const routes = express.Router();
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "c7d799d5851043",
+      pass: "f2a8a5060d9f5d"
+    }
+  });
+
+routes.post('/feedbacks', async (req, res)=>{
+    const {type, comment, screenshot}= req.body;
+    
+    const feedback = await prisma.feedbacks.create({
+        data:{
+            type,
+            comment,
+            screenshot,
+        }
+    })
+
+    transport.sendMail({
+        from: 'Gasparzinho <Gasp@ghost.com>',
+        to: 'Lucas Daniel<lucasdanielrambo@gmail.com>',
+        subject:'Novo feedback',
+        html:[
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+                `<p>Tipo de feedback: ${type}</p>`,
+                `<p>Comentário: ${comment}</p>`,
+            `</div>`
+        ].join('\n')
+    });
+
+    
+    return res.status(201).json({data: feedback});
+})
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,48 +1,15 @@
 import express from 'express'
-import nodemailer from 'nodemailer'
-import { prisma } from './prisma';
+
+import { routes } from './routes'
+
+
+
 
 const app= express();
 
 app.use(express.json())
+app.use(routes)
 
-const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "c7d799d5851043",
-      pass: "f2a8a5060d9f5d"
-    }
-  });
-
-
-
-app.post('/feedbacks', async (req, res)=>{
-    const {type, comment, screenshot}= req.body;
-    
-    const feedback = await prisma.feedbacks.create({
-        data:{
-            type,
-            comment,
-            screenshot,
-        }
-    })
-
-    transport.sendMail({
-        from: 'Gasparzinho <Gasp@ghost.com>',
-        to: 'Lucas Daniel<lucasdanielrambo@gmail.com>',
-        subject:'Novo feedback',
-        html:[
-            `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
-                `<p>Tipo de feedback: ${type}</p>`,
-                `<p>Comentário: ${comment}</p>`,
-            `</div>`
-        ].join('\n')
-    });
-
-    
-    return res.status(201).json({data: feedback});
-})
 
 app.listen(3333, ()=>{
     console.log('HTTP server running!')
