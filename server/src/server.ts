8c0fe3588b1a866b6635591937fdb29bf3f1328f
LCSDNL <lucasdanielrambo@gmail.com>
2022-05-04 20:34:15 -0300

    add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express'
 
 import { routes } from './routes'
@@ -7,8 +8,9 @@ import { routes } from './routes'
 
 const app= express();
 
-app.use(express.json())
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 
 app.listen(3333, ()=>{
