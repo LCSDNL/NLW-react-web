02326ab6aad9206b3d5fcb3f32885f7fecbab009
LCSDNL <lucasdanielrambo@gmail.com>
2022-05-06 21:23:00 -0300

    cahged export default Widget.tsx

--- react-web/src/components/widget.tsx
+++ react-web/src/components/widget.tsx
@@ -3,7 +3,7 @@ import { ChatTeardropDots } from 'phosphor-react';
 import { WidgetForm } from './WidgetForm';
 
 
-export function Widget(){
+ export default function Widget(){
 
     return (
         <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
